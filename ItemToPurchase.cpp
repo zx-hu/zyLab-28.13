c2cef24a1fd305e7df1f1cde989dfa28ea37a97c
zx-hu <167228682+zx-hu@users.noreply.github.com>
2024-04-17 13:45:06 -0500

    Update ItemToPurchase.cpp
    Implemented all setters and getters
    TODO: constructor

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -2,3 +2,35 @@
 using namespace std;
 
 #include "ItemToPurchase.h"
+
+//TODO: Constructor
+ItemToPurchase::ItemToPurchase(){
+}
+
+//Set name
+void ItemToPurchase::SetName(string n){
+  name = n;
+}
+
+//Get name
+string ItemToPurchase::GetName(){
+  return name;
+}
+
+//Set price
+void ItemToPurchase::SetPrice(int p)
+  price = p;
+}
+
+//Get price
+int ItemToPurchase::GetPrice(){
+  return price;
+}
+
+void ItemToPurchase::SetQuantity(int q){
+  quantity = q;
+}
+
+int ItemToPurchase::GetQuantity(){
+  return quantity;
+}
