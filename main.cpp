25e62abc79b600a7e575225b46471b2d0079ef52
zx-hu <167228682+zx-hu@users.noreply.github.com>
2024-04-19 12:38:05 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,40 @@ using namespace std;
 //update from user-2
 
 int main(){
+  //get params for the two items
+  string name1, name2;
+  int price1, price2, quant1, quant2;
+  //item 1
+  cout << "Item 1" << endl;
+  cout << "Enter the item name:" << endl;
+  cin >> name1;
+  cout << "Enter the item price:" << endl;
+  cin >> price1;
+  cout << "Enter the item quantity:" << endl;
+  cin >> quant1;
+  
+  //item 2
+  cout << endl << "Item 2" << endl;
+  cout << "Enter the item name:" << endl;
+  cin >> name2;
+  cout << "Enter the item price:" << endl;
+  cin >> price2;
+  cout << "Enter the item quantity:" << endl;
+  cin >> quant2;
 
+  //create the items
+  item1 = ItemToPurchase(name1, price1, quant1);
+  item2 = ItemToPurchase(name2, price2, quant2);
+  
+  //output
+  int total1, total2;
+  total1 = item1.GetQuantity() * item1.GetPrice();
+  total2 = item2.GetQuantity() * item2.GetPrice();
+  
+  cout << "TOTAL COST" << endl;
+  cout << item1.GetName() << " " << item1.GetQuantity() << " @ $" << item1.GetPrice() << " = $" << total1 << endl;
+  cout << item2.GetName() << " " << item2.GetQuantity() << " @ $" << item2.GetPrice() << " = $" << total2 << endl;
+  cout << endl;
+  cout << "Total: $" << total1 + total2 << endl;
   return 0;
 }
